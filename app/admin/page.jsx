4b5f7f77233547ccd27927d81e5d2867dcc2423b
Vaibhav Tupe <141541491+vaibhav0513@users.noreply.github.com>
2024-10-10 17:30:42 +0530

    fixed some bugs

--- app/admin/page.jsx
+++ app/admin/page.jsx
@@ -1,11 +1,38 @@
-import React from 'react'
+'use client';  
 
-const page = () => {
+import React, { useState } from 'react';
+
+const Page = () => {
+  const [state, setState] = useState();
+
   return (
     <div>
-      
+      {/* Your component JSX here */}
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default Page;
+
+
+
+// import React, {useState} from 'react'
+
+// const page = () => {
+//   return (
+//     <div>
+      
+//     </div>
+//   )
+// }
+// const Page = () => {
+//   const [state, setState] = useState(initialState);
+
+//   useEffect(() => {
+//     // Logic here
+//   }, [dependency]);
+// }
+
+
+// export default Page
+
--- app/blogs/[id]/page.jsx
+++ app/blogs/[id]/page.jsx
@@ -1,61 +1,142 @@
 'use client';
-import { assets, blog_data } from '@/Assets/assets';
+import { assets } from '@/Assets/assets';
 import Footer from '@/Components/Footer';
 import Image from 'next/image';
 import Link from 'next/link';
-import React, { useEffect, useState } from 'react'
-import axios from 'axios'
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
-const page = ({params}) => {
+const Page = ({ params }) => {
   const [data, setData] = useState(null);
 
   const fetchBlogData = async () => {
-    const response = await axios.get('/api/blog',{
-      params:{
-        id:params.id
-      }
-    })
-    setData(response.data);
-  }
+    try {
+      const response = await axios.get('/api/blog', {
+        params: { id: params.id },
+      });
+      setData(response.data);
+    } catch (error) {
+      console.error("Error fetching blog data:", error);
+    }
+  };
 
-  useEffect(()  => {
+  useEffect(() => {
     fetchBlogData();
-  },[])
+  }, [params.id]); // Dependency on params.id to refetch on ID change
 
-  return ( data ? <>
-    <div className='bg-gray-200 py-5 px-5 md:px-12 lg:px-28'>
-      <div className='flex justify-between items-center'>
-        <Link href='/'>
-          <Image src={assets.logo1} width={190} alt='' className='w-[130px] sm:w-auto'/>
-        </Link>
-        <button className='flex items-center gap-2 font-medium py-1 px-3 sm:py-3 sm:px-6 border border-black shadow-[-7px_7px_0px_#f95959]'>Get Started <Image src={assets.arrow} alt=''/></button>
-      </div>
-      <div className='text-center my-24'>
-        <h1 className='text-2xl sm:text-5xl text-[#e84a5f] font-semibold max-w-[700px] mx-auto'>{data.title}</h1>
-        <Image className='mx-auto mt-6 border border-white rounded-full' src={data.authorImg} width={60} height={60} alt='Author-Img'/>
-        <p className='mt-1 pb-2 text-lg max-w-[740px] mx-auto '>{data.author}</p> 
-      </div>
-    </div>
-    <div className='mx-5 max-w-[800px] md:mx-auto mt-[-100px] mb-10' >
-      <Image className='border-4 border-white' src={data.image} width={1280} height={720} alt='Blog-image'/>
-      <div className='blog-content' dangerouslySetInnerHTML={{__html:data.description}}>
+  return (
+    data ? (
+      <>
+        <div className='bg-gray-200 py-5 px-5 md:px-12 lg:px-28'>
+          <div className='flex justify-between items-center'>
+            <Link href='/'>
+              <Image src={assets.logo1} width={190} alt='Logo' className='w-[130px] sm:w-auto' />
+            </Link>
+            <button className='flex items-center gap-2 font-medium py-1 px-3 sm:py-3 sm:px-6 border border-black shadow-[-7px_7px_0px_#f95959]'>
+              Get Started <Image src={assets.arrow} alt='Arrow Icon' />
+            </button>
+          </div>
+          <div className='text-center my-24'>
+            <h1 className='text-2xl sm:text-5xl text-[#e84a5f] font-semibold max-w-[700px] mx-auto'>{data.title}</h1>
+            <Image className='mx-auto mt-6 border border-white rounded-full' src={data.authorImg} width={60} height={60} alt='Author' />
+            <p className='mt-1 pb-2 text-lg max-w-[740px] mx-auto'>{data.author}</p>
+          </div>
+        </div>
+        <div className='mx-5 max-w-[800px] md:mx-auto mt-[-100px] mb-10'>
+          <Image className='border-4 border-white' src={data.image} width={1280} height={720} alt='Blog image' />
+          <div className='blog-content' dangerouslySetInnerHTML={{ __html: data.description }} />
+          <div className='my-24'>
+            <p className='text-black font-semibold my-4'>Share this article on social media</p>
+            <div className='flex'>
+              <Image src={assets.facebook_icon} width={50} alt='Facebook Icon' />
+              <Image src={assets.twitter_icon} width={50} alt='Twitter Icon' />
+              <Image src={assets.googleplus_icon} width={50} alt='Google Plus Icon' />
+            </div>
+          </div>
+        </div>
+        <Footer />
+      </>
+    ) : (
+      <div>Loading...</div> // Show loading message while data is being fetched
+    )
+  );
+};
 
-      </div>
+export default Page;
+
+
+
+
+// 'use client';
+// import { assets, blog_data } from '@/Assets/assets';
+// import Footer from '@/Components/Footer';
+// import Image from 'next/image';
+// import Link from 'next/link';
+// import React, { useEffect, useState } from 'react'
+// import axios from 'axios'
+
+// const page = ({params}) => {
+//   const [data, setData] = useState(null);
+
+//   // const fetchBlogData = async () => {
+//   //   const response = await axios.get('/api/blog',{
+//   //     params:{
+//   //       id:params.id
+//   //     }
+//   //   })
+//   //   setData(response.data);
+//   // }
+
+//   // useEffect(()  => {
+//   //   fetchBlogData();
+//   // },[])
+
+//   const fetchBlogData = async () => {
+//     const response = await axios.get('/api/blog', {
+//       params: { id: params.id }
+//     });
+//     setData(response.data);
+//   };
+
+//   useEffect(() => {
+//     fetchBlogData();
+//   }, [params.id]);  // Add the dependency
+// }
+
+//   return ( data ? <>
+//     <div className='bg-gray-200 py-5 px-5 md:px-12 lg:px-28'>
+//       <div className='flex justify-between items-center'>
+//         <Link href='/'>
+//           <Image src={assets.logo1} width={190} alt='' className='w-[130px] sm:w-auto'/>
+//         </Link>
+//         <button className='flex items-center gap-2 font-medium py-1 px-3 sm:py-3 sm:px-6 border border-black shadow-[-7px_7px_0px_#f95959]'>Get Started <Image src={assets.arrow} alt=''/></button>
+//       </div>
+//       <div className='text-center my-24'>
+//         <h1 className='text-2xl sm:text-5xl text-[#e84a5f] font-semibold max-w-[700px] mx-auto'>{data.title}</h1>
+//         <Image className='mx-auto mt-6 border border-white rounded-full' src={data.authorImg} width={60} height={60} alt='Author-Img'/>
+//         <p className='mt-1 pb-2 text-lg max-w-[740px] mx-auto '>{data.author}</p> 
+//       </div>
+//     </div>
+//     <div className='mx-5 max-w-[800px] md:mx-auto mt-[-100px] mb-10' >
+//       <Image className='border-4 border-white' src={data.image} width={1280} height={720} alt='Blog-image'/>
+//       <div className='blog-content' dangerouslySetInnerHTML={{__html:data.description}}>
+
+//       </div>
       
 
 
-      <div className='my-24'>
-        <p className='text-black font font-semibold my-4'>Share this article on social media </p>
-        <div className='flex'>
-          <Image src={assets.facebook_icon} width={50} alt=''/>
-          <Image src={assets.twitter_icon} width={50} alt=''/>
-          <Image src={assets.googleplus_icon} width={50} alt=''/>
-        </div>
-      </div>
-    </div>
-    <Footer/>
-    </>:<></>
-  )
-}
+//       <div className='my-24'>
+//         <p className='text-black font font-semibold my-4'>Share this article on social media </p>
+//         <div className='flex'>
+//           <Image src={assets.facebook_icon} width={50} alt=''/>
+//           <Image src={assets.twitter_icon} width={50} alt=''/>
+//           <Image src={assets.googleplus_icon} width={50} alt=''/>
+//         </div>
+//       </div>
+//     </div>
+//     <Footer/>
+//     </>:<></>
+//   )
+// }
 
-export default page
+// export default page
